0cd30a8b2438b477b8691c8e8de0d252fed15685
devdrqgon <70473449+devdrqgon@users.noreply.github.com>
2021-11-16 08:35:48 +0100

    comment code

--- src/features/budget/budgetCalculator.core.ts
+++ src/features/budget/budgetCalculator.core.ts
@@ -1,18 +1,38 @@
 const getRestMoney = (gross: number, sumBills: number, food: number, misc: number) => {
-        
-   
 
-    return gross - sumBills - ( (food *4)  + (misc * 4)) 
+
+
+    return gross - sumBills - ((food * 4) + (misc * 4))
 
 }
 
 
-const getWeeklyBudget = ( food: number, misc: number) =>{
+const getWeeklyBudget = (food: number, misc: number) => {
     return food + misc
 }
 
 
-export default{
+export default {
     getRestMoney,
     getWeeklyBudget
 }
+
+
+
+
+/**
+ *
+
+// Dear User,
+
+// after excluding  D, C and B from A
+// You can save E by having a weekly budget of F
+
+
+I wanna buy:
+        * bed stuff which costs 500
+        * Permis which costs 2000
+        * Buffer Money 3000
+
+
+ */
