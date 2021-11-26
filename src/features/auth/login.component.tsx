422c922b61b4dd810f2c6d20857f75086c5050ea
devdrqgon <70473449+devdrqgon@users.noreply.github.com>
2021-11-26 13:07:48 +0100

    new login ui

--- src/features/auth/login.component.tsx
+++ src/features/auth/login.component.tsx
@@ -5,6 +5,12 @@ import { useHistory } from 'react-router'
 import { UserContext } from "contexts/user.context"
 import { Link } from 'react-router-dom'
 import toast from 'react-hot-toast'
+import { Button } from '@chakra-ui/button'
+import { useColorModeValue } from '@chakra-ui/color-mode'
+import { FormControl, FormLabel } from '@chakra-ui/form-control'
+import { Input } from '@chakra-ui/input'
+import { Flex, Stack, Heading, Box, Text } from '@chakra-ui/layout'
+import { Checkbox } from '@chakra-ui/react'
 
 export default function LoginPage() {
     const [username, setUsername] = React.useState<string>('')
@@ -58,86 +64,61 @@ export default function LoginPage() {
         }
     }
     return (
-        <div
-            style={{
-                display: 'grid',
-                gridTemplateColumns: '1fr 1fr',
-                gridTemplateRows: '1fr',
-                gap: '0px 0px',
-                gridTemplateAreas: `
-                'login login'
-                `,
-                height: '95vh',
-                width: '100wh',
-            }}
-        >
-            <div
-                style={{
-                    gridArea: 'login',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center'
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50', 'gray.800')}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+                    <Text fontSize={'lg'} color={'gray.600'}>
+                        {/* to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️ */}
+                    </Text>
+                </Stack>
+                <Box
+                    rounded={'lg'}
+                    bg={useColorModeValue('white', 'gray.700')}
+                    boxShadow={'lg'}
+                    p={8}>
+                    <Stack spacing={4}>
+                        <FormControl id="email">
+                            <FormLabel>Username</FormLabel>
+                            <Input onChange={(e: ChangeEvent<HTMLInputElement>) => { setUsername(e.target.value as unknown as string) }} type="text" />
+                        </FormControl>
+                        <FormControl id="password">
+                            <FormLabel>Password</FormLabel>
+                            <Input onChange={(e: ChangeEvent<HTMLInputElement>) => { setPassword(e.target.value as unknown as string) }} type="password" />
+                        </FormControl>
+                        <Stack spacing={10}>
+                            <Stack
+                                direction={{ base: 'column', sm: 'row' }}
+                                align={'start'}
+                                justify={'space-between'}>
+                                <Checkbox>Remember me</Checkbox>
 
+                            </Stack>
+                            <Button
+                                onClick={loginClicked}
+                                bg={'blue.400'}
+                                color={'white'}
+                                _hover={{
+                                    bg: 'blue.500',
+                                }}>
+                                Sign in
+                            </Button>
+                            <Link to={"/register"}>
+                                <Text fontSize={'lg'} color={'#2ad'}>
+                                    New to MoneyCoacht? Register here!
+                                </Text>
+                            </Link>
 
-                }}>
-                <div
-                    style={{
-                        display: 'grid',
-                        // backgroundColor: 'yellow',
-                        gridAutoColumns: '1fr',
-                        gridTemplateColumns: '1fr',
-                        gridTemplateRows: '0.5fr 2.2fr 0.3fr',
-                        gap: '0px 0px',
-                        gridTemplateAreas: `
-                                    'title'
-                                    'main'
-                                    'footer'
-                                `
-                        ,
-                    }}>
-                    <div
-                        style={{
-                            gridArea: 'title',
-                            display: 'flex',
-                            justifyContent: 'center'
-                        }}>
-                        Login
-
-
-                    </div>
-                    <div
-                        style={{
-                            gridArea: 'main',
-                            display: 'flex',
-                            flexDirection: 'column'
-                        }}>
-                        <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => { setUsername(e.target.value as unknown as string) }} />
-                        <input type="password" onChange={(e: ChangeEvent<HTMLInputElement>) => { setPassword(e.target.value as unknown as string) }} />
-
-                        <button onClick={loginClicked} >
-                            Sign In
-                        </button>
-                        <div
-                            style={{
-                                color: 'red',
-                                display: 'flex',
-                                justifyContent: 'center',
-                                marginTop: '20px'
-                            }}>
-                            {uiErr}
-
-                        </div>
-                    </div>
-
-                    <div
-                        style={{ gridArea: 'footer' }}>
-                        <Link to={"/register"}>No Account? Register here!</Link>
-                        <br />
-                    </div>
-                </div>
-            </div>
-
-
-        </div >
-    )
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Flex>
+    );
 }
+
+
